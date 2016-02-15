f72349f2beb053db407901dc985f478466815c00
Patrick Tchankue <ptchankue@gmail.com>
2016-02-15 11:07:10 +0200

    Creating a model for Todo

--- todo_service/todo_app/models.py
+++ todo_service/todo_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Todo(models.Model):
+    # username of the author of the Todo's task
+    author = models.CharField(max_length=50)
+    description = models.TextField()
+    created_at = models.DateTimeField()
+    due_at = models.DateTimeField()
+    completed = models.BooleanField(default=False)
