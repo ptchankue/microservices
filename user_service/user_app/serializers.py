0cb4cbc4a642a40b1dcfb3b193cab375481fed5e
Patrick Tchankue <ptchankue@gmail.com>
2016-02-15 15:22:21 +0200

    Creating serializers for signup and login

---
+++ user_service/user_app/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'password', 'email', 'first_name', 'last_name')
+
+class LoginSerializer(serializers.Serializer):
+
+    username = serializers.CharField(required = True)
+    password = serializers.CharField(required = True)
