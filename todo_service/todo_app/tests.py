69ebd142c4d07fd9eafaf3cfd96acafa4a87192b
Patrick Tchankue <ptchankue@gmail.com>
2016-02-15 11:07:30 +0200

    Creating a tests for the todo app

--- todo_service/todo_app/tests.py
+++ todo_service/todo_app/tests.py
@@ -1,3 +1,39 @@
 from django.test import TestCase
 
+import json
+from datetime import datetime
 # Create your tests here.
+
+
+class TodoTestCase(TestCase):
+
+    """>>> testing todo app"""
+    def setUp(self):
+        pass
+    def test_add(self):
+        """>>> Creating a todo task"""
+        data = {
+            "author": "myuser",
+            "description": "Sprint planning meeting",
+            "created_at":datetime.now()
+        }
+        url="api/v1/todos/"
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, 201)
+
+    def test_retrieve(self):
+        """>>> Retrieving a todo task"""
+        pass
+
+    def test_edit(self):
+        """>>> Editing a todo task"""
+        pass
+    def test_delete(self):
+        """>>> Deleting a todo task"""
+        pass
+
+    def test_listing(self):
+        """>>> Listing a todo tasks"""
+        url="api/v1/todos/"
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
