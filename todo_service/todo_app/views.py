755c3e5ddfda66a4f89582a1801f9fa55f9d6454
Patrick Tchankue <ptchankue@gmail.com>
2016-02-15 11:11:34 +0200

    Implementing the views

--- todo_service/todo_app/views.py
+++ todo_service/todo_app/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
+from todo_app.models import Todo
+
+from todo_app.serializers import TodoSerializer
 # Create your views here.
+
+
+def home(request):
+    msg = "You have landed on the Todo service :)"
+    return HttpResponse(msg)
+class TodoViewSet(viewsets.ModelViewSet):
+
+    """API endpoing to manage todo lists"""
+
+    serializer_class = TodoSerializer
+    queryset = Todo.objects.all()
+
+    def create(self, request):
+        pass
+
+    def update(self, request):
+        pass
+
+    def retrieve(self, request):
+        pass
+
+    def destroy(self, request):
+        pass
+        
