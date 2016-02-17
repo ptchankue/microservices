a4fc6d26f64079db28cfc95e7e872b993e4abc16
Patrick Tchankue <ptchankue@gmail.com>
2016-02-17 22:52:46 +0200

    Adding a serializer for updating a user

--- user_service/user_app/serializers.py
+++ user_service/user_app/serializers.py
@@ -9,6 +9,13 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ('id', 'username', 'password', 'email', 'first_name', 'last_name')
 
+class UserUpdateSerializer(serializers.Serializer):
+    """
+        Defining the User serializer for User creation and manipulation
+    """
+    first_name = serializers.CharField(required=False)
+    last_name = serializers.CharField(required=False)
+
 class LoginSerializer(serializers.Serializer):
     """
         Defining the Login serializer for user input
